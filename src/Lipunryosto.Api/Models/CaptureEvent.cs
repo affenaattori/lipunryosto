5663cbe388e78bbfaf2196105cd84ccf0478fd79
affenaattori <affenaattori@hotmail.com>
2025-08-13 00:43:06 +0300

    Add files via upload

--- src/Lipunryosto.Api/Models/CaptureEvent.cs
+++ src/Lipunryosto.Api/Models/CaptureEvent.cs
@@ -1,16 +1,23 @@
-
 using System.ComponentModel.DataAnnotations;
-namespace Lipunryosto.Api.Models;
-public class CaptureEvent {
-  [Key] public Guid Id {get;set;}=Guid.NewGuid();
-  [Required] public Guid GameId {get;set;}
-  [Required] public Guid FlagId {get;set;}
-  [Required] public Guid TeamId {get;set;}
-  public Guid? DeviceId {get;set;}
-  public DateTimeOffset Timestamp {get;set;}=DateTimeOffset.UtcNow;
-  public string Phase {get;set;}="start";
-  public int PointsAwarded {get;set;}=0;
-  public double? Lat {get;set;}
-  public double? Lon {get;set;}
-  public double? Accuracy {get;set;}
+
+namespace Lipunryosto.Api.Models
+{
+    public class CaptureEvent
+    {
+        [Key] public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required] public Guid GameId { get; set; }
+        [Required] public Guid FlagId { get; set; }
+        [Required] public Guid TeamId { get; set; }
+
+        // "start" tai "confirm"
+        [MaxLength(20)]
+        public string Phase { get; set; } = "start";
+
+        public double? Lat { get; set; }
+        public double? Lon { get; set; }
+        public double? Accuracy { get; set; }
+
+        public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.UtcNow;
+    }
 }
--- src/Lipunryosto.Api/Models/Device.cs
+++ src/Lipunryosto.Api/Models/Device.cs
@@ -1,14 +1,30 @@
-
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-namespace Lipunryosto.Api.Models;
-public class Device {
-  [Key] public Guid Id {get;set;}=Guid.NewGuid();
-  [Required] public Guid GameId {get;set;}
-  [ForeignKey(nameof(GameId))] public Game Game {get;set;}=default!;
-  public string? Name {get;set;}
-  public string? DeviceTokenHash {get;set;}
-  public Guid? AssignedFlagId {get;set;}
-  public DateTimeOffset LastSeen {get;set;}=DateTimeOffset.UtcNow;
-  public bool IsActive {get;set;}=true;
+
+namespace Lipunryosto.Api.Models
+{
+    public class Device
+    {
+        [Key] public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid? GameId { get; set; }
+        public Guid? AssignedFlagId { get; set; }
+
+        [MaxLength(120)]
+        public string? Name { get; set; }
+
+        [MaxLength(100)]
+        public string? PwaInstanceId { get; set; }
+
+        [MaxLength(100)]
+        public string? Token { get; set; }
+
+        [MaxLength(160)]
+        public string? DeviceTokenHash { get; set; }
+
+        public double? Lat { get; set; }
+        public double? Lon { get; set; }
+        public double? AccuracyMeters { get; set; }
+        public DateTimeOffset? LastSeen { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
 }
