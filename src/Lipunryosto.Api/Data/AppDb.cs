b99bb84f873600d594871273324067cd5b272f90
affenaattori <affenaattori@hotmail.com>
2025-08-13 00:21:38 +0300

    Add files via upload

--- src/Lipunryosto.Api/Data/AppDb.cs
+++ src/Lipunryosto.Api/Data/AppDb.cs
@@ -10,5 +10,7 @@ namespace Lipunryosto.Api.Data
         public DbSet<Game> Games => Set<Game>();
         public DbSet<Team> Teams => Set<Team>();
         public DbSet<FlagPoint> Flags => Set<FlagPoint>();
+        public DbSet<Device> Devices => Set<Device>();
+        public DbSet<GameEvent> Events => Set<GameEvent>();
     }
 }
