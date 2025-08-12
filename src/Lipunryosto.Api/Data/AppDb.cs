45d476b401d63f9c868efbc7fad959b12c21f0f1
affenaattori <affenaattori@hotmail.com>
2025-08-12 23:46:20 +0300

    Add files via upload

--- src/Lipunryosto.Api/Data/AppDb.cs
+++ src/Lipunryosto.Api/Data/AppDb.cs
@@ -1,12 +1,14 @@
-
 using Lipunryosto.Api.Models;
 using Microsoft.EntityFrameworkCore;
-namespace Lipunryosto.Api.Data;
-public partial class AppDb : DbContext {
-  public AppDb(DbContextOptions<AppDb> options):base(options) {}
-  public DbSet<Game> Games => Set<Game>();
-  public DbSet<Team> Teams => Set<Team>();
-  public DbSet<FlagPoint> Flags => Set<FlagPoint>();
-  public DbSet<Device> Devices => Set<Device>();
-  public DbSet<CaptureEvent> Events => Set<CaptureEvent>();
+
+namespace Lipunryosto.Api.Data
+{
+    public partial class AppDb : DbContext
+    {
+        public AppDb(DbContextOptions<AppDb> options) : base(options) {}
+
+        public DbSet<Game> Games => Set<Game>();
+        public DbSet<Team> Teams => Set<Team>();
+        public DbSet<FlagPoint> Flags => Set<FlagPoint>();
+    }
 }
