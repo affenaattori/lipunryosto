6d88184fa700fe561367a3553a2b008491b6b49a
affenaattori <affenaattori@hotmail.com>
2025-08-13 13:23:27 +0300

    Update AppDb.cs

--- src/Lipunryosto.Api/Data/AppDb.cs
+++ src/Lipunryosto.Api/Data/AppDb.cs
@@ -5,14 +5,12 @@ namespace Lipunryosto.Api.Data
 {
     public partial class AppDb : DbContext
     {
-        public AppDb(DbContextOptions<AppDb> options) : base(options) {}
+        public AppDb(DbContextOptions<AppDb> options) : base(options) { }
 
         public DbSet<Game> Games => Set<Game>();
         public DbSet<Team> Teams => Set<Team>();
         public DbSet<FlagPoint> Flags => Set<FlagPoint>();
         public DbSet<Device> Devices => Set<Device>();
         public DbSet<CaptureEvent> Events => Set<CaptureEvent>();
-        public DbSet<FlagPoint> Flags { get; set; }
-
     }
 }
