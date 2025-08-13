2709e140ceeb2aeef1141a221a58fe0b8d47bbc7
affenaattori <affenaattori@hotmail.com>
2025-08-13 13:05:39 +0300

    Update AppDb.cs

--- src/Lipunryosto.Api/Data/AppDb.cs
+++ src/Lipunryosto.Api/Data/AppDb.cs
@@ -12,5 +12,7 @@ namespace Lipunryosto.Api.Data
         public DbSet<FlagPoint> Flags => Set<FlagPoint>();
         public DbSet<Device> Devices => Set<Device>();
         public DbSet<CaptureEvent> Events => Set<CaptureEvent>();
+        public DbSet<FlagPoint> Flags { get; set; }
+
     }
 }
