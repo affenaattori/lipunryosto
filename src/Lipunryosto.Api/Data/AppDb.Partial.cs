3568295e941782d9bc4834e126aec337848c0131
affenaattori <affenaattori@hotmail.com>
2025-08-13 13:26:40 +0300

    Update AppDb.Partial.cs

--- src/Lipunryosto.Api/Data/AppDb.Partial.cs
+++ src/Lipunryosto.Api/Data/AppDb.Partial.cs
@@ -9,21 +9,36 @@ namespace Lipunryosto.Api.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            // Game -> Teams
             modelBuilder.Entity<Game>()
                 .HasMany(g => g.Teams)
                 .WithOne(t => t.Game)
                 .HasForeignKey(t => t.GameId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            // Game -> Flags
             modelBuilder.Entity<Game>()
                 .HasMany(g => g.Flags)
                 .WithOne(f => f.Game)
                 .HasForeignKey(f => f.GameId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            // Oletuksia
             modelBuilder.Entity<Game>()
                 .Property(g => g.WinCondition)
                 .HasDefaultValue("MostPointsAtTime");
+
+            modelBuilder.Entity<Team>()
+                .Property(t => t.Score)
+                .HasDefaultValue(0);
+
+            modelBuilder.Entity<FlagPoint>()
+                .Property(f => f.Status)
+                .HasMaxLength(24);
+
+            modelBuilder.Entity<FlagPoint>()
+                .Property(f => f.Color)
+                .HasMaxLength(24);
         }
     }
 }
